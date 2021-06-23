8d5f95ad68a89ed68cc3f608099fe970ee5c342d
mario-galindo <mgalindo9410@gmail.com>
2021-06-22 21:00:12 -0600

    change app port

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 
 app.get('/',(req, res)=> res.send("Server with express!"))
 
-app.listen(3000)
-console.log("Server on port 3000")
+app.listen(8080)
+console.log("Server on port 8080")
