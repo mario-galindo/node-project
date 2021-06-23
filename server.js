cfe936dfb184bcd86e4e4e7b1174bc7117ec373f
mario-galindo <mgalindo9410@gmail.com>
2021-06-22 21:03:15 -0600

    returning a html code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express();
 
-app.get('/',(req, res)=> res.send("Server with express!"))
+app.get('/',(req, res)=> res.send("<h1>Hello from node by HTML</h1>"))
 
 app.listen(8080)
 console.log("Server on port 8080")
