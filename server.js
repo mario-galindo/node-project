9b604c8edfbbbb343051146148be38ec475fc8ec
mario-galindo <mgalindo9410@gmail.com>
2021-11-14 19:07:53 -0600

    new message added to identify tag

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express();
 
-app.get('/',(req, res)=> res.send("<h1>Hello from node by HTML</h1>"))
+app.get('/',(req, res)=> res.send("<h1>Hello from node by HTML | new v1.0</h1>"))
 
 app.listen(3000)
 console.log("Server on port 3000")
