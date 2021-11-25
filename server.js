f727456c240ada039e0262cec6e4638c9736ac12
mario-galindo <mgalindo9410@gmail.com>
2021-11-25 11:58:47 -0600

    new versiones added for testing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express();
 
-app.get('/',(req, res)=> res.send("<h1>Hello from node by HTML | new v2.1</h1>"))
+app.get('/',(req, res)=> res.send("<h1>Hello from node by HTML | new v2.2</h1>"))
 
 app.listen(3000)
 console.log("Server on port 3000")
