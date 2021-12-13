16567d19734a86c7292f5aa4da74af3317f4b9a4
mario-galindo <mgalindo9410@gmail.com>
2021-12-13 11:54:30 -0600

    add module to handle env variables

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require("express");
-const dotenv = require("dotenv");
 
 const app = express();
-dotenv.config();
-
-const PORT = process.env.PORT;
-const NODE_ENV = process.env.NODE_ENV;
+const { port, node_env } = require("./config");
 
 app.get("/", (req, res) =>
   res.send(
-    `<p>Node.js Backend <br><strong>Environment</strong> : ${NODE_ENV} <br><strong>Host</strong>: ${req.hostname}</p>`
+    `<p>Node.js Backend <br><strong>Environment</strong> : ${node_env} <br><strong>Host</strong>: ${req.hostname}</p>`
   )
 );
 
-app.listen(PORT);
-console.log(`Server on port: ${PORT}, Environment: ${NODE_ENV}`);
+app.listen(port);
+console.log(`Server on port: ${port}, Environment: ${node_env}`);
