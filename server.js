53380f064a496a9337b5117f2e9701b21bca0128
mario-galindo <mgalindo9410@gmail.com>
2021-12-13 17:39:00 -0600

    instead of returning a html returning a json data

--- server.js
+++ server.js
@@ -4,9 +4,26 @@ const app = express();
 const { port, node_env } = require("./config");
 
 app.get("/", (req, res) =>
-  res.send(
-    `<p>Node.js Backend <br><strong>Environment</strong> : ${node_env} <br><strong>Host</strong>: ${req.hostname}</p>`
-  )
+  res.send([
+    {
+      Name: "Mario",
+      City: "Berlin",
+      Environment: node_env,
+      Host: req.hostname,
+    },
+    {
+      Name: "Daniela",
+      City: "Miami",
+      Environment: node_env,
+      Host: req.hostname,
+    },
+    {
+      Name: "Thomas",
+      City: "Hamburg",
+      Environment: node_env,
+      Host: req.hostname,
+    },
+  ])
 );
 
 app.listen(port);
